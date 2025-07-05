924e8a1863da458b8e4edc25faa5bd2ed6a3c430
lijinhong11 <tygfhk@outlook.com>
2025-07-05 16:30:53 +0800

    add postgresql support

--- src/main/java/io/github/lijinhong11/mdatabase/enums/DatabaseType.java
+++ src/main/java/io/github/lijinhong11/mdatabase/enums/DatabaseType.java
@@ -3,7 +3,8 @@ package io.github.lijinhong11.mdatabase.enums;
 public enum DatabaseType {
     SQLITE,
     MYSQL,
-    MARIADB;
+    MARIADB,
+    POSTGRESQL;
 
     public static DatabaseType getByName(String name) {
         for (DatabaseType value : values()) {
--- src/main/java/io/github/lijinhong11/mdatabase/impl/SQLConnections.java
+++ src/main/java/io/github/lijinhong11/mdatabase/impl/SQLConnections.java
@@ -18,4 +18,8 @@ public final class SQLConnections {
     public static DatabaseConnection mariadb(String host, int port, String database, String username, String password, DatabaseParameters parameters) {
         return new MariaDBConnection(host, port, database, username, password, parameters);
     }
+
+    public static DatabaseConnection postgresql(String host, int port, String database, String username, String password, DatabaseParameters parameters) {
+        return new PostgreSQLConnection(host, port, database, username, password, parameters);
+    }
 }
